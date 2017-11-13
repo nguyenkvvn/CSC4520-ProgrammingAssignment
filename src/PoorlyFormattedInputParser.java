b79dc5517f5870b1c947d152e0b7a2910bf1a196
Vinh Nguyen <nguyen.kvvn@gmail.com>
2017-11-13 04:30:24 -0500

    Refactor and working Parser

--- src/PoorlyFormattedInputParser.java
+++ src/PoorlyFormattedInputParser.java
@@ -13,7 +13,9 @@ import org.jgrapht.graph.SimpleDirectedWeightedGraph;
 public class PoorlyFormattedInputParser
 {
     String filepath;
-    String[] AbominableOutput;
+    //String[] AbominableOutput;
+    
+    ArrayList<String> AbominableOutput;
 
     public PoorlyFormattedInputParser()
     {
@@ -29,48 +31,28 @@ public class PoorlyFormattedInputParser
     public void loadFile(String fp) throws IOException
     {
         filepath = fp;
-        //CITATION: http://www.homeandlearn.co.uk/java/read_a_textfile_in_java.html
-        //FileReader fr = new FileReader(filepath);
-        //BufferedReader textReader = new BufferedReader(fr);
-        //end citation
         
         File file = new File(filepath);
         Scanner textReader = new Scanner(file);
         
-        //scope out the number of lines
-//        int linescount = 0;
-//        while (textReader.readLine()!= null)
+//        int i = 0;
+//        while (textReader.hasNextLine())
 //        {
-//            linescount++;
-//        }
-//        
-//        //reload the BufferReader to grab the lines this time and dump it into an array of Strings. (All this could be avoided if the input was given as a CSV or with some sense used in the formatting.)
-//        textReader = new BufferedReader(fr);
-//        AbominableOutput = new String[linescount];
-//        for (int i = 0; i < linescount; i++)
-//        {
-//            AbominableOutput[i] = textReader.readLine();
-//            System.out.println("Reading " + AbominableOutput[i]);
-//            
+//            //AbominableOutput[i] = textReader.nextLine();
+//            System.out.println("Reading " + textReader.nextLine());
+//            i++;
 //        }
         
+        //textReader = new Scanner(file);
+        AbominableOutput = new ArrayList();
         int i = 0;
         while (textReader.hasNextLine())
         {
-            //AbominableOutput[i] = textReader.nextLine();
-            System.out.println("Reading " + textReader.nextLine());
+            AbominableOutput.add(textReader.nextLine());
+            //System.out.println("Adding " + AbominableOutput.get(i));
             i++;
         }
-        
-        textReader = new Scanner(file);
-        AbominableOutput = new String[i];
-        i = 0;
-        while (textReader.hasNextLine())
-        {
-            AbominableOutput[i] = textReader.nextLine();
-            System.out.println("Adding " + AbominableOutput[i]);
-            //i++;
-        }
+        System.out.println(AbominableOutput);
         textReader.close();
     }
     
@@ -92,61 +74,78 @@ public class PoorlyFormattedInputParser
         //LOGIC FOR PARSING STRINGS
         //ignore the first line
         //Adding the vertices first so that no errors occure when defining the edges
-        int settedVertex = -1;
-        for (int i = 2; i < AbominableOutput.length; i++)
+        String settedVertex = "";
+        for (int i = 1; i < AbominableOutput.size(); i++)
         {
+            //System.out.println("Parsing Line " + i + ": " + AbominableOutput.get(i));
             //VERTEX CONDITION
             //if the line begins with a non space (i.e, an integer)
-//            if (AbominableOutput[i].charAt(0) != ' ')
+            if (!AbominableOutput.get(i).isEmpty())
+            {
+                if (AbominableOutput.get(i).charAt(0) != ' ')
+                {
+                    graphfromfile.addVertex(AbominableOutput.get(i));
+                    settedVertex = AbominableOutput.get(i);
+                    //System.out.println("Added " + AbominableOutput.get(i) + " as a vertex.");
+                }
+            }
+            else
+            {
+                //System.out.println("Parsing Line " + i + ": was am empty line!");
+            }
+            
+            
+//            //extract the integers out of the line
+//            //CITATION: https://stackoverflow.com/questions/12721261/how-to-extract-multiple-integers-from-a-string-in-java
+//            System.out.println("Parsing] Line " + i + ": " + AbominableOutput.get(i));
+//            Matcher m = Pattern.compile("\\d+/g").matcher(AbominableOutput.get(i));
+//            try
 //            {
-//                graphfromfile.addVertex(AbominableOutput[i]);
-//                settedVertex = Integer.parseInt(AbominableOutput[i]);
-//                System.out.println("Added " + AbominableOutput[i] + " as a vertex.");
+//                System.out.println(m.group());
+//                //settedVertex = Integer.parseInt(m.group());
+//                graphfromfile.addVertex(AbominableOutput.get(i));
+//                System.out.println("Added] " + AbominableOutput.get(i) + " as a vertex.");
+//               // int vertex = Integer.parseInt(AbominableOutput.get(i));
 //            }
-            
-            //extract the integers out of the line
-            //CITATION: https://stackoverflow.com/questions/12721261/how-to-extract-multiple-integers-from-a-string-in-java
-            System.out.println("Parsing: Line " + i + " " + AbominableOutput[i]);
-            Matcher m = Pattern.compile("\\d+").matcher(AbominableOutput[i]);
-            settedVertex = Integer.parseInt(m.group(1));
-            graphfromfile.addVertex(AbominableOutput[i]);
-            System.out.println("Added " + AbominableOutput[i] + " as a vertex.");
-            
-            
-            int vertex = Integer.parseInt(AbominableOutput[i]);
+//            catch (Exception e)
+//            {
+//                //do nothing if there isn't a match.
+//                System.out.println("Line " + i + ": " + AbominableOutput.get(i) + " was empty.");
+//            }
         }
         
-        for (int i = 1; i < AbominableOutput.length; i++)
+        for (int i = 1; i < AbominableOutput.size(); i++)
         {
             //VERTEX CONDITION
             //if the line begins with a non space (i.e, an integer)
-            if (AbominableOutput[i].charAt(0) != ' ')
+            if (AbominableOutput.get(i).charAt(0) != ' ')
             {
                 //logic reused to set the vertex for edging- no need to add again into the graph
                 //graphfromfile.addVertex("Vertex " + AbominableOutput[i]);
-                settedVertex = Integer.parseInt(AbominableOutput[i]);
+                settedVertex = AbominableOutput.get(i);
             }
             //VERTEX EDGE CONDITION
             //if the line begins with a space
-            if (AbominableOutput[i].charAt(0) == ' ')
+            if (AbominableOutput.get(i).charAt(0) == ' ')
             {
-                int endVertex = -1;
+                String endVertex = "";
                 int pathWeight = -1;
                 
                 //extract the integers out of the line
                 //CITATION: https://stackoverflow.com/questions/12721261/how-to-extract-multiple-integers-from-a-string-in-java
-                Matcher m = Pattern.compile("\\d+").matcher(AbominableOutput[i]);
+                Matcher m = Pattern.compile("\\d+").matcher(AbominableOutput.get(i));
+                System.out.println(i + " " + AbominableOutput.get(i));
                 while(m.find())
                 {
                     //the first integer found is the end vertex
-                    if (endVertex == -1)
+                    if (endVertex.isEmpty())
                     {
-                        endVertex = Integer.parseInt(m.group());
+                        endVertex = m.group(0);
                     }
                     //the second (and last) integer found is the weight
                     else
                     {
-                        pathWeight = Integer.parseInt(m.group());
+                        pathWeight = Integer.parseInt(m.group(0));
                     }
                 }
                 
--- src/ProgrammingAssignment.java
+++ src/ProgrammingAssignment.java
@@ -1,5 +1,6 @@
 
 import java.io.IOException;
+import org.jgraph.graph.Edge;
 import org.jgrapht.graph.DefaultWeightedEdge;
 import org.jgrapht.graph.SimpleDirectedWeightedGraph;
 
@@ -32,7 +33,33 @@ public class ProgrammingAssignment
         SimpleDirectedWeightedGraph<String, DefaultWeightedEdge> graph = parser.makeGraph();
         
         //begin the timer when computing hte algorithm, not when reading the abominable file
+        
+        //
 
+    }
+    
+    public static int DijkstraSumShortestPath(SimpleDirectedWeightedGraph g)
+    {
+        //traversed vertexes
+        ArrayList
+        g.edgesOf("0");
+        g.vertexSet();
+        
+        //Vertex Set
+            //copy each item in the vertex set to an ArrayList
+        //Vertext Set - Lowest Weight
+            //parallel array list with 9999 weight
+            //but node 0 has 0 weight
+        
+        //for all vertexes in the set
+            //for the current node (start at 0, with distance 0) and its edges
+               //go through all its edges
+               //if the distance of the current vertex to the 
+                //set the termating vertex's distance of the edge to the node's distance
+                
+        //sum up all of the weights
+        //*why? because we are told to take the "shortest paths from node 0 to each node" which can be interpreted as the sum of all vertex weights bc we are touching EVERY single node
+        return 0;
     }
 
 }
